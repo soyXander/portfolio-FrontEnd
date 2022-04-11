f0c5e90e39855efc0228841bb0a1251d698d34e3
Xander.- <zn.xander@gmail.com>
2022-04-11 03:28:45 -0300

    - Cambio de posición del menu Sidebar. - Esquinas redondeadas hacia afuera en item menu activo. - Toggle para expandir y contraer el menu agregado.

--- Front-End-Angular/src/app/components/header/sidebar/sidebar.component.ts
+++ Front-End-Angular/src/app/components/header/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { faBars, faX, faHouse, faUser, faStar, faGraduationCap, faUserGear, faBriefcase, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,8 +9,53 @@ import { Component, OnInit } from '@angular/core';
 
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Iconos
+   */
+  // Toggle
+  faBars = faBars;
+  faX = faX;
 
+  // Menu
+  faHouse = faHouse;
+  faUser = faUser;
+  faStar = faStar;
+  faGraduationCap = faGraduationCap;
+  faUserGear = faUserGear;
+  faBriefcase = faBriefcase;
+  faPaperPlane = faPaperPlane;
+
+
+  constructor(private renderer: Renderer2, private elem: ElementRef){}
+
+  /**
+   * Función para abrir y cerrar el menu cuando se clickea el toggle.
+   */
+  toggleAction(){
+    let menuToggle = this.elem.nativeElement.querySelector('.toggle');
+    let sidebar = this.elem.nativeElement.querySelector('.sidebar')
+    menuToggle.onclick = function(){
+      menuToggle.classList.toggle('active');
+      sidebar.classList.toggle('active');
+    }
+  }
+
+  /**
+   * Función que lista el elemento .list y agregar la clase 'active' cuando se clickea,
+   * mientras que tambien se remueve la misma clase a los demas elementos.
+   */
+  markActive(){
+    const list = this.elem.nativeElement.querySelectorAll('.list');
+    for(let i = 0; i < list.length; i++){
+      list[i].onclick = function(){
+        let j = 0;
+        while(j < list.length){
+          list[j++].className = 'list';
+        }
+        list[i].className = 'list active';
+      }
+    }
+  }
   ngOnInit(): void {
 
   }
