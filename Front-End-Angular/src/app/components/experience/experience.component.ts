6789f3da97ac22a3cff2f3c4df3e1b1e9824e32f
Xander.- <zn.xander@gmail.com>
2022-04-19 23:17:51 -0300

    Componente Experiencia: - Estilo definido.

--- Front-End-Angular/src/app/components/experience/experience.component.ts
+++ Front-End-Angular/src/app/components/experience/experience.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
+
 export class ExperienceComponent implements OnInit {
+
+  faStar = faStar;
 
   constructor() { }
 
